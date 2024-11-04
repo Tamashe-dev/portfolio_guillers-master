2e244c431440d83325287f6c178f913a201687a9
tamashe-dev <sofyan93130@gmail.com>
2024-11-04 21:03:24 +0100

    allo francais

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ export default function About() {
     return(
         <section id="about" className="mt-10 min-h-screen flex p-4 h-screen flex-col gap-6">
             <div className="bg-blue-600 md:mt-10 h-1/2 w-1/2">
-                <p>Etudiant en école d'ingénieur, j'ai appris à bla bla bla je suis raciste bla bla bla bla bla bla</p>
+                <p>Etudiant en école d'ingénieur à l'ESIEE Paris, j'ai pu experimenter avec Java, Python et C durant ma scolarité: 
+                    j'ai du créer un jeu vidéo en Java et faire plusieurs dizaines d'exercices en Python et C. 
+                    Durant mon temps libre j'ai également eu le temps d'apprendre à utiliser Javascript, notamment le framework React avec lequel ce site a été codé.
+                    Passionné par l'informatique, l'Histoire, la linguistique, l'anthropologie ainsi que les jeux vidéos j'essaie d'associer du mieux que je peux ces différentes passions.
+                    Vous trouverez ci-dessous mon CV et mes différents projets aboutis ou non.
+                </p>
+            </div>
+            <div className="bg-black h-28 w-12">
+                Curriculum Vitae
             </div>
         </section>
     )
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,29 +1,38 @@
-import { ChevronDoubleUpIcon } from "@heroicons/react/16/solid";
 import { BrowserRouter , Link } from 'react-router-dom';
 import LinkedInImage from "../LinkedIn.png";
 import GitHubImage from "../GitHub.png";
+import PhoneImage from "../Phone.png";
+import GmailImage from "../Gmail.png";
 
 export default function Contact() {
     return (
-        <section id="contact" className="h-screen flex flex-col">
-            <div className="flex flex-col gap-10 p-10">
-                <div className="text-white text-5xl flex justify-center mt-10 p-4 top-0">Comment me contacter?</div>
-                <div className="p-4 flex flex-col gap-8">
-                    <BrowserRouter className="flex flex-row">
-                        <Link to="https://www.linkedin.com/in/sofyan-guillermet-laouad/" target="_blank" className="w-fit">
-                            <img src={LinkedInImage} alt="linkedinimage" className="hover:scale-105 border-black border-4 border-opacity-0 hover:border-opacity-50"/>
-                        </Link>
-                    </BrowserRouter>
-                    <BrowserRouter className="flex flex-row">
-                        <Link to="https://github.com/Tamashe-dev" target="_blank" className="w-fit">
-                            <img src={GitHubImage} alt="githubimage" className="hover:scale-105 border-black border-4 border-opacity-0 hover:border-opacity-50"/>
-                        </Link>
-                    </BrowserRouter>
-                    <div className="bg-green-500 text-white w-fit flex flex-row text-2xl">Téléphone/WhatsApp:</div>
-                    <div className="bg-red-900 text-white w-fit flex flex-row text-2xl">E-Mail:</div>
+        <section id="contact" className=" flex flex-col">
+            <div className="flex flex-col h-screen gap-10 p-10">
+                <div className="text-white text-7xl flex justify-center mt-10 p-4 top-0">Comment me contacter?</div>
+                <div className="flex flex-col">
+                    <div className="p-20 flex justify-center justify-items-center gap-64 flex-row">
+                        <BrowserRouter className="flex">
+                            <Link to="https://www.linkedin.com/in/sofyan-guillermet-laouad/" target="_blank" className="w-fit">
+                                <img src={LinkedInImage} width={150} heigth={250} alt="linkedinimage" className="hover:scale-105"/>
+                            </Link>
+                        </BrowserRouter>
+                        <BrowserRouter className="flex">
+                            <Link to="https://github.com/Tamashe-dev" target="_blank" className="w-fit">
+                                <img src={GitHubImage} width={150} heigth={250} alt="githubimage" className="hover:scale-105 rounded-full"/>
+                            </Link>
+                        </BrowserRouter>
+                    </div>
+                    <div className="p-20 flex justify-center justify-items-center gap-64 flex-row">
+                        <a href="tel:+33669736032">
+                            <img src={PhoneImage} width={150} heigth={250} alt="phoneimage" className="hover:scale-105"/> 
+                        </a>
+                        <a href="mailto:sofyan.guillermet-laouad@edu.esiee.fr">
+                            <img src={GmailImage} width={150} heigth={250} alt="gmailimage" className="hover:scale-105"/>
+                        </a>
+                    </div>
                 </div>
             </div>
-            <div className="bg-black flex flex-col bottom-0 h-1/2 justify-center items-center gap-4">
+            <div className="bg-black flex flex-col bottom-0 h-40 justify-center items-center gap-4">
                 <div className="text-white hover:animate-bouncer hover:scale-125 hover:animate-pulse">
                     <a href="#accueil">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
