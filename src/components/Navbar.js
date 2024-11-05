0a5e3d075020b42a2bbc816560c219d31797e43e
tamashe-dev <sofyan93130@gmail.com>
2024-11-05 19:23:28 +0100

    BITE BITE

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,22 +2,22 @@ export default function Navbar() {
     return (
         <section className="w-screen fixed top-0 bg-black">
             <div className="flex flex-row px-2 text-white justify-center">
-                <div className="bg-black p-2 hover:animate-pulse hover:bg-gray-800 w-36 text-center hover:border-t hover:border-white hover:scale-110">
+                <div className="p-2 hover:animate-pulse hover:bg-gray-800 w-36 text-center hover:border-b-2 hover:border-white hover:scale-110">
                     <a href="#about">
                         A propos de moi
                     </a>
                 </div>
-                <div className="bg-black p-2 hover:animate-pulse hover:bg-gray-800 w-36 text-center hover:border-t hover:border-white hover:scale-110">
+                <div className="p-2 hover:animate-pulse hover:bg-gray-800 w-36 text-center hover:border-b-2 hover:border-white hover:scale-110">
                     <a href="#skills" >
                         Compétences
                     </a>
                 </div>
-                <div className="bg-black p-2 hover:animate-pulse hover:bg-gray-800 w-36 text-center hover:border-t hover:border-white hover:scale-110">
+                <div className="p-2 hover:animate-pulse hover:bg-gray-800 w-36 text-center hover:border-b-2 hover:border-white hover:scale-110">
                     <a href="#projects" >
                         Projets
                     </a>
                 </div>
-                <div className="bg-black p-2 hover:animate-pulse hover:bg-gray-800 w-36 text-center hover:border-t hover:border-white hover:scale-110">
+                <div className="p-2 hover:animate-pulse hover:bg-gray-800 w-36 text-center hover:border-b-2 hover:border-white hover:scale-110">
                     <a href="#contact" >
                         Contacts
                     </a>
